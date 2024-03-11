19c1cd5cd71ea077fe17f2f24b6ba79ae0a5fd01
hongchun <jhc5317@gmail.com>
2024-03-11 19:22:58 +0900

    헤더 임시

--- front/src/Components/Header.jsx
+++ front/src/Components/Header.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+import CategoryButton from "./header/CategoryButton";
+import SearchBox from "./header/SearchBox";
+
 const Header = () => {
   return (
-    <header className="w-[1280px]">
-      <span className="text-xLarge">Header</span>
+    <header className="w-[1280px] h-[80px] flex items-center">
+      <CategoryButton />
+      <Link to="/" className="text-primary text-4xl">
+        중고는 가격을 찢어
+      </Link>
+      <SearchBox />
     </header>
   );
 };
