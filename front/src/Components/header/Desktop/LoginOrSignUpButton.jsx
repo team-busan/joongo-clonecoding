292bbe8f167ee73ed0465bf7b5ca94d508e25037
hongchun <jhc5317@gmail.com>
2024-03-22 15:51:30 +0900

    데스크탑 헤더 반응형 수정

--- front/src/Components/header/Desktop/LoginOrSignUpButton.jsx
+++ front/src/Components/header/Desktop/LoginOrSignUpButton.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LoginOrSignUpButton = () => {
   return (
     <Link to="/sign?form=in" className="flex items-center gap-1">
-      <MdLogin className="text-xl xl:text-2xl" />
+      <MdLogin className="text-xl desktop:text-2xl" />
       <span>로그인/회원가입</span>
     </Link>
   );
