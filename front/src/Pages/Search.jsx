85eb0513829a3200c2744a70a47f2e19d2557a73
Lee jae hyeong <ftko6252@naver.com>
2024-03-22 16:53:13 +0900

    test

--- front/src/Pages/Search.jsx
+++ front/src/Pages/Search.jsx
@@ -7,9 +7,9 @@ import SearchProduct from '../Components/SearchComponents/SearchProduct';
 const Search = () => {
   const [loading, setLoading] = useState(true);
   const [searchText] = useSearchParams();
-  const text = searchText.get('search');
+  const text = searchText.get('keyword');
   const [products, setProducts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState({ main: "", sub: "", type : ""});
+  const [selectedCategory, setSelectedCategory] = useState({ main: "", sub: "", product_status : "", lowPrice : 0, highPrice : 0});
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,31 +26,35 @@ const Search = () => {
     fetchData();
   }, [searchText]);
 
-  console.log(products);
-  console.log(text);
-
   const filterProducts = (searchText, selectedCategory) => {
     let filtered = products.filter(product => product.title.includes(searchText));
-
-    // 카테고리에 따라 필터링
+  
     if (selectedCategory.main) {
       filtered = filtered.filter(product => product.main_category === selectedCategory.main);
       if (selectedCategory.sub) {
         filtered = filtered.filter(product => product.sub_category === selectedCategory.sub);
       }
     }
-
     // 유형에 따라 필터링
-    if (selectedCategory.type !== "") {
-      if(selectedCategory.type === 0) {
-        filtered = filtered.filter(product => product.type === 0);
-      } else if(selectedCategory.type === 1) {
-        filtered = filtered.filter(product => product.type === 1);
+    if (selectedCategory.product_status !== "") {
+      if(selectedCategory.product_status === "새상품") {
+        filtered = filtered.filter(product => product.product_status === "새상품");
+      } else if(selectedCategory.product_status === "중고") {
+        filtered = filtered.filter(product => product.product_status === "중고");
       }
     }
-
     return filtered;
   };
+
+  const onPriceChange = () => {
+    let filteredProducts = [...products];
+    filteredProducts = filterProducts(text, selectedCategory);
+    filteredProducts = filteredProducts.filter(product =>
+      (selectedCategory.lowPrice <= product.price &&
+        product.price <= selectedCategory.highPrice)
+    );
+    setProducts(filteredProducts);
+  }
 
   if (loading) {
     return <div>Loading...</div>;
@@ -61,7 +65,9 @@ const Search = () => {
   return (
     <div className="p-20">
       <h2 className="mb-2">검색결과 &quot;{text}&quot;</h2>
-      <SearchFilter setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory}/>
+      <SearchFilter
+        setSelectedCategory={setSelectedCategory} selectedCategory={selectedCategory}
+        onPriceChange = {onPriceChange}/>
       <p className="mt-10 mb-5">현재 페이지의 상품 가격을 비교해봤어요</p>
       <SearchProduct products={filteredProducts}/>
     </div>
